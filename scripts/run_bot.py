f15e74171e562b6cbe8cca6b0134f2347c935de1
Leonidas2035 <leonidas2035@users.noreply.github.com>
2026-06-16 19:35:40 +0300

    chore: update config, scripts, ops, tools, UI, and CI

--- scripts/run_bot.py
+++ scripts/run_bot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from quantum_edge_core.bot.service import TradingBot
+from quantum_edge_core.bot.service import BotService as TradingBot
 from quantum_edge_core.utils.async_runner import run_service
 from quantum_edge_core.logging_setup import setup_logging
 
@@ -7,7 +7,7 @@ from quantum_edge_core.logging_setup import setup_logging
 def main():
     setup_logging()
     bot = TradingBot()
-    run_service(bot._runner_wrapper())
+    run_service(bot.run())
 
 
 if __name__ == "__main__":
